64c64eba22731431a09aa19197bf5a46dd1f2176
Alex Yarmarkovich <alexy@bwh.harvard.edu>
2014-06-25 13:25:57 -0400

    BUG: fixed tests and they run now.

--- FinslerBacktracing/Testing/FinslerBacktracing_Compare.cxx
+++ FinslerBacktracing/Testing/FinslerBacktracing_Compare.cxx
@@ -1,13 +1,13 @@
-#include "itkWin32Header.h"
-#include <iostream>
-#include <fstream>
-#include "itkNumericTraits.h"
-#include "itkImageFileReader.h"
-#include "itkImageRegionConstIterator.h"
-
-#include <itkFactoryRegistration.h>
-
-int RegressionTestImage (const char *, const char *, int, bool);
+// VTK include to write poly-data:
+#include <vtkNew.h>
+#include <vtkSmartPointer.h>
+#include <vtkTrivialProducer.h>
+#include <vtkVersion.h>
+#include <vtkAlgorithmOutput.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkPolyData.h>
+#include <vtkDataArray.h>
+#include <vtkPointData.h>
 
 int main(int argc, char **argv)
 {
@@ -16,50 +16,62 @@ int main(int argc, char **argv)
       std::cerr << "testImage, compareImage" << std::endl;
       return -1;
    }
-   
-   itk::itkFactoryRegistration();
+   vtkSmartPointer<vtkXMLPolyDataReader> reader1 = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+   vtkSmartPointer<vtkXMLPolyDataReader> reader2 = vtkSmartPointer<vtkXMLPolyDataReader>::New();
 
-   typedef float PixelType;
-   const unsigned int DIMENSION = 3;
-   typedef itk::Image<PixelType,DIMENSION>          ImageType;
-   typedef itk::ImageFileReader<ImageType>          ReaderType;
-   typedef itk::ImageRegionConstIterator<ImageType> IteratorType;
-   
-   ReaderType::Pointer reader1 = ReaderType::New();
-   ReaderType::Pointer reader2 = ReaderType::New();
    reader1->SetFileName( argv[1] );
    reader2->SetFileName( argv[2] );
-   
-   try{
+
+   try
+   {
       reader1->Update();
       reader2->Update();
    }
-   catch(itk::ExceptionObject& e){
-      std::cerr << "Error updating readers: " << e.GetDescription() << std::endl;
+   catch(...)
+   {
+      std::cerr << "Error updating readers: "<< std::endl;
       return EXIT_FAILURE;
    }
-   
-   for( unsigned int k=0; k<DIMENSION; ++k ){
-      if( reader1->GetOutput()->GetLargestPossibleRegion().GetSize()[k] != reader2->GetOutput()->GetLargestPossibleRegion().GetSize()[k] ){
-         std::cerr << "Mismatch in dimension: " << k;
-         return EXIT_FAILURE;
-      }
+
+   vtkPolyData * poly1 = vtkPolyData::SafeDownCast(reader1->GetOutputPort()->GetProducer()->GetOutputDataObject(
+      reader1->GetOutputPort()->GetIndex()));
+   vtkPolyData * poly2 = vtkPolyData::SafeDownCast(reader2->GetOutputPort()->GetProducer()->GetOutputDataObject(
+      reader2->GetOutputPort()->GetIndex()));
+
+  if (poly1 == 0 || poly2 == 0)
+   {
+     std::cerr << "Error: poly is 0"<< std::endl;
+     return EXIT_FAILURE;
    }
-   
-   IteratorType it1( reader1->GetOutput(), reader1->GetOutput()->GetLargestPossibleRegion() );
-   IteratorType it2( reader2->GetOutput(), reader2->GetOutput()->GetLargestPossibleRegion() );
-   
-   double difference = itk::NumericTraits<double>::Zero;
-   for( it1.GoToBegin(),it2.GoToBegin(); !it1.IsAtEnd(); ++it1,++it2 ){
-      float temp = it1.Get() - it2.Get();
-      difference += ( temp>itk::NumericTraits<float>::Zero ? temp : -temp );
+
+   vtkDataArray *ar1 = poly1->GetPointData()->GetScalars();
+   vtkDataArray *ar2 = poly2->GetPointData()->GetScalars();
+
+  if (ar1 == 0 || ar2 == 0)
+   {
+     std::cerr << "Error: scalar is 0"<< std::endl;
+     return EXIT_FAILURE;
    }
-   
-   if( difference>1e-9 ){
-      std::cerr << "Error (" << difference << ") is above tolerance (1e-9)";
+
+   if (poly1->GetNumberOfVerts() != poly2->GetNumberOfVerts())
+   {
+      std::cerr << "Error in GetNumberOfVerts"<< std::endl;
       return EXIT_FAILURE;
    }
-   else{
-      return EXIT_SUCCESS;
+
+   int n = ar1->GetNumberOfTuples();
+   for (int i=0; i< n; i++)
+   {
+     double *v1 = ar1->GetTuple(i);
+     double *v2 = ar2->GetTuple(i);
+     double difference = fabs(*v1 - *v2);
+
+     if( difference>1e-9 )
+     {
+        std::cerr << "Error (" << difference << ") is above tolerance (1e-9)"<< std::endl;
+        return EXIT_FAILURE;
+     }
    }
+
+   return EXIT_SUCCESS;
 }
